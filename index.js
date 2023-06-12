ba303841b1c244fc0f01531c54d2b310b2dc4348
Maryam <maryamjaved.xo@gmail.com>
2023-06-12 22:02:44 +0500

    update

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-  // hamburger
-
-  let ham = document.getElementById('hamburger');
+let ham = document.getElementById('hamburger');
   let expend=document.getElementById('expend');
   let expendul=document.getElementById('expul');
-  function toggleopacity(){
-      if(expend.style.opacity===1){
-          expend.style.opacity=0;
-      }
+ 
 
-      else{
-          expend.style.opacity=1;
-      }
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var ham = document.getElementById('hamburger');
+  
+    function toggleopacity(){
+        if(expend.style.opacity===1){
+            expend.style.opacity=0;
+        }
+  
+        else{
+            expend.style.opacity=1;
+        }
+  
+    }
+  
+  function toggleicon(){
+  if(ham.classList.contains('open')){
+      ham.classList.remove('open');
+      ham.classList.add('cross');
   }
-
-  function togglevisibility(){
-      if(expend.style.visibility==='hidden'){
-          expend.style.visibility='visible';
-      }
-
-      else{
-          expend.style.visibility='hidden';
-      }
+  
+  else{
+      ham.classList.remove('cross');
+      ham.classList.add('open');
   }
-
-  ham.addEventListener('click', ()=>{
-       toggleopacity();
-       togglevisibility();
-  })
+  }
+  
+  
+    function togglevisibility(){
+        if(expend.style.visibility==='hidden'){
+            expend.style.visibility='visible';
+        }
+  
+        else{
+            expend.style.visibility='hidden';
+        }
+    }
+  
+    ham.addEventListener('click', ()=>{
+   
+        togglevisibility();
+        toggleopacity();
+        toggleicon();
+   })
+   
+  });
